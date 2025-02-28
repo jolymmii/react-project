1c79bdda4e973fde6c0952fd89075a4729a0735f
jolymmii <yerbolkassymov27@gmail.com>
2025-02-28 13:11:52 +0500

    исправил момент с мокапи

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import App from './App'
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
 	<React.StrictMode>
-		<Router basename='/react-project'>
+		<Router basename='/react-project/'>
 			<App />
 		</Router>
 	</React.StrictMode>
